9d0ad51a7375ff26c642c1b1a050b3be43e9d7dd
Sanjarbek17 <sanjarsaidov173@gmail.com>
2025-03-15 15:16:54 +0500

    default pages

---
+++ lib/features/chat/presentation/screens/chat_account.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatAccount extends StatelessWidget {
+  const ChatAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Chat Account')), body: Center(child: Text('Chat Account')));
+  }
+}
